7fade65cf1ae9df0dff7c738a4c3f91c05b4f404
Erik <erik.nilsson@live.com>
2015-10-09 10:48:53 +0200

    Added user interface

--- addAndSubtract/Form1.Designer.cs
+++ addAndSubtract/Form1.Designer.cs
@@ -28,20 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubstract = new System.Windows.Forms.Button();
+            this.lblSum = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(132, 302);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(137, 63);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnSubstract
+            // 
+            this.btnSubstract.Location = new System.Drawing.Point(332, 302);
+            this.btnSubstract.Name = "btnSubstract";
+            this.btnSubstract.Size = new System.Drawing.Size(137, 63);
+            this.btnSubstract.TabIndex = 1;
+            this.btnSubstract.Text = "substract";
+            this.btnSubstract.UseVisualStyleBackColor = true;
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(265, 147);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(52, 25);
+            this.lblSum.TabIndex = 2;
+            this.lblSum.Text = "sum";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(658, 463);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.btnSubstract);
+            this.Controls.Add(this.btnAdd);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubstract;
+        private System.Windows.Forms.Label lblSum;
     }
 }
 
